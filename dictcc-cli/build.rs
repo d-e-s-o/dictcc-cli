5ea25bf4435ee68376dd278b8c657a59fbcbbee6
Daniel Mueller <deso@posteo.net>
2018-01-07 17:56:55 -0800

    Properly handle more meta data in curly and angle brackets
    In addition to meta data in square brackets there are two more forms of
    meta data: an indication whether something is singular or plural (e.g.,
    {sg.}) and abbreviations (e.g., <EUR>).
    To correctly handle those cases, this change adds an additional
    conditions to our SQL query that catch all possible combinations.
    Unfortunately, we encounter combinatorial explosion here and even more
    unfortunately, there currently is seemingly no standardized way to
    iterate over all permutations of, say, a set in Rust, without relying on
    other crates. To that end, we take the rough road of manually specifying
    all combinations.

---
+++ dictcc-cli/build.rs
@@ -0,0 +1,32 @@
+// build.rs
+
+// *************************************************************************
+// * Copyright (C) 2018 Daniel Mueller (deso@posteo.net)                   *
+// *                                                                       *
+// * This program is free software: you can redistribute it and/or modify  *
+// * it under the terms of the GNU General Public License as published by  *
+// * the Free Software Foundation, either version 3 of the License, or     *
+// * (at your option) any later version.                                   *
+// *                                                                       *
+// * This program is distributed in the hope that it will be useful,       *
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+// * GNU General Public License for more details.                          *
+// *                                                                       *
+// * You should have received a copy of the GNU General Public License     *
+// * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+// *************************************************************************
+
+use std::process::Command;
+use std::path::Path;
+
+fn main() {
+  let py_file = Path::new(&"src").join("permutations.py");
+  let out_file = Path::new(&"src").join("permutations.in");
+
+  Command::new("python3")
+    .arg(py_file)
+    .arg(out_file)
+    .status()
+    .unwrap();
+}
